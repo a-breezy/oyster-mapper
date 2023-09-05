11e40e9fe1d9b237a6b1373ba3c21bf0f98bf890
a.breezy <ambrose.wilkins@gmail.com>
2023-09-05 17:28:23 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Dummy data for demonstration
+oysters = [
+    {"id": 0, "farm_name": "Oysther", "location": {"city":"Georgetown", "state":"Maine", "zipcode":"04903", "coordinates": {"lat": "0321", "long": "1230"}, "species": "the best", "description": "A delicious blend of sweetness and salinity", "tasting_notes": ["sweet", "salinity"], "size":"medium"}}
+]
+
+# Create
+@app.route('/oysters', methods=['POST'])
+def add_oyster():
+    new_oyster = request.get_json()
+    oysters.append(new_oyster)
+    return jsonify({"message": "oyster created"}), 201
+
+# Read (Get all oysters)
+@app.route('/oysters', methods=['GET'])
+def get_oysters():
+    return jsonify(oysters)
+
+# Read (Get a specific oyster by ID)
+@app.route('/oysters/<int:id>', methods=['GET'])
+def get_oyster(id):
+    oyster = next((oyster for oyster in oysters if oyster["id"] == id), None)
+    if oyster:
+        return jsonify(oyster)
+    return jsonify({"message": "oyster not found"}), 404
+
+# Update
+@app.route('/oysters/<int:id>', methods=['PUT'])
+def update_oyster(id):
+    oyster = next((oyster for oyster in oysters if oyster["id"] == id), None)
+    if oyster:
+        updated_data = request.get_json()
+        oyster.update(updated_data)
+        return jsonify({"message": "oyster updated"})
+    return jsonify({"message": "oyster not found"}), 404
+
+# Delete
+@app.route('/oysters/<int:id>', methods=['DELETE'])
+def delete_oyster(id):
+    oyster = next((oyster for oyster in oysters if oyster["id"] == id), None)
+    if oyster:
+        oysters.remove(oyster)
+        return jsonify({"message": "oyster deleted"})
+    return jsonify({"message": "oyster not found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ connection.py
@@ -0,0 +1,17 @@
+# connection to mysql db
+
+#! first install dependencies
+#? pip install Flask-SQLAlchemy mysql-connector-python
+
+
+#? from flask_sqlalchemy import SQLAlchemy
+
+#? app = Flask(__name__)
+
+#? # Configure the MySQL database connection
+#? app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://username:password@localhost/databasename'
+#? app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#? db = SQLAlchemy(app)
+
+#! export to app.py
